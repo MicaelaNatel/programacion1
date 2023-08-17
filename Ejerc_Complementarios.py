bb719a3a641b6d82cf05ba38833675d69798ab50
MicaelaNatel <141734634+MicaelaNatel@users.noreply.github.com>
2023-08-16 23:26:25 -0300

    Ejercicios Complementarios

---
+++ Ejerc_Complementarios.py
@@ -0,0 +1,52 @@
+#Ejercicio 1
+numero1=int(21)
+#Ejercicio 2
+numero2=float(2.6)
+#Ejercicio 3
+suma=numero1+numero2
+print("La suma es: ",suma)
+#Ejercicio 4
+resta=numero1+numero2
+mult=numero1*numero2
+div=numero1/numero2
+print("La resta es: ", resta)
+print("La multiplicacion es: ",mult)
+print("La division es: ",div)
+#Ejercicio 5 
+nombre="Micaela"
+#Ejercicio 6
+precio=int(3000)
+#Ejercicio 7
+descuento=0.25
+#Ejercicio 8
+precio_final=float(precio-(precio*descuento))
+print(precio_final)
+#Ejercicio 9
+cadena="programacion I"
+print(cadena)
+#Ejercicio 10
+Longitud=len(cadena)
+print(Longitud)
+#Ejercicio 11
+precio= 25.8
+print(int(precio))
+#Ejercicio 12
+nombre="Micaela"
+apellido="Natel"
+nombre_completo=nombre+" "+apellido
+print(nombre_completo)
+#Ejercicio 13 
+edad=18
+edad_final=(int(edad+5-10))
+print(edad_final)
+#Ejercicio 14
+altura=1.63
+altura_final=(altura*4)/3
+print(float(altura_final))
+#Ejercicio 15
+nombre_may= "MICAELA NATEL"
+nombre_min=nombre_may.lower()
+print(nombre_may)
+#Ejercicio 16
+primera_may=nombre_may[0] + nombre_may[1:].lower()
+print(primera_may)
